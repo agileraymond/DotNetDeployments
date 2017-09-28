b2024e2944cb7d86e70bbed3fdfd09dc7c714ada
Raymond Sanchez <raymond.sanchez@verizon.com>
2017-09-28 06:21:38 -0500

    added aws codedeploy package

---
+++ DotNetDeployments/Controllers/DeploymentController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DotNetDeployments.Models;
+using Amazon.CodeDeploy;
+
+namespace DotNetDeployments.Controllers
+{
+    public class DeploymentController : Controller
+    {
+        public IActionResult Index()
+        {            
+            var codeDeploy = new AmazonCodeDeployClient();
+
+            return View();
+        }      
+    }
+}
