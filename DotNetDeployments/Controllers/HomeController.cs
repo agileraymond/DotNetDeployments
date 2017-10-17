22add56772330f7e4ce71838bfc0e90ad277de22
KevinLamb <kevinlambert46@gmail.com>
2017-10-16 22:09:41 -0400

    Readded home page

---
+++ DotNetDeployments/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using DotNetDeployments.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DotNetDeployments.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
