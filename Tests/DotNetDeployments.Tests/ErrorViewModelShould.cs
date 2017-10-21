c6b2cf71bf04c6fc30585cbe7540ee855ae9471c
Muhammad Ihsan <mihsan191@gmail.com>
2017-10-21 22:33:18 +1100

    Add unit test for ErrorViewModel

---
+++ Tests/DotNetDeployments.Tests/ErrorViewModelShould.cs
@@ -0,0 +1,24 @@
+﻿using DotNetDeployments.Models;
+using Xunit;
+
+namespace DotNetDeployments.Tests
+{
+    public class ErrorViewModelShould
+    {
+        [Theory]
+        [InlineData("", false)]
+        [InlineData(null, false)]
+        [InlineData(" ", true)]
+        [InlineData("1a", true)]
+        [InlineData(" 1a", true)]
+        public void ReferToRequestIdToGetShowRequestId(string requestId, bool expectedShowRequestId)
+        {
+            var errorViewModel = new ErrorViewModel
+            {
+                RequestId = requestId
+            };
+
+            Assert.Equal(expectedShowRequestId, errorViewModel.ShowRequestId);
+        }
+    }
+}
