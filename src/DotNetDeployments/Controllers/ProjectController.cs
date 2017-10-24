6ed6d9e890ea59581c8c2c37aae2ad33223cdf14
Raymond Sanchez <raymond.sanchez@verizon.com>
2017-10-24 17:24:24 -0500

    added aws dynamodb package, and initial code

--- src/DotNetDeployments/Controllers/ProjectController.cs
+++ src/DotNetDeployments/Controllers/ProjectController.cs
@@ -1,13 +1,38 @@
 ﻿using DotNetDeployments.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
 
 namespace DotNetDeployments.Controllers
 {
     public class ProjectController : Controller
     {
+        IAmazonDynamoDB AmazonDynamoDbClient { get; set; }
+
+        public ProjectController(IAmazonDynamoDB amazonDynamoDbClient)
+        {
+            AmazonDynamoDbClient = amazonDynamoDbClient;            
+        }
+
         public IActionResult Index()
         {
+            // Define item attributes
+            Dictionary<string, attributevalue=""> attributes = new Dictionary<string, attributevalue="">();
+            
+            attributes["SolutionName"] = new AttributeValue { S = "Mdb.Logger" };
+            attributes["ProjectName"] = new AttributeValue { S = "Mdb.Utility" };
+            attributes["ProjectType"] = new AttributeValue { N = "1" };
+            
+            // Create PutItem request
+            PutItemRequest request = new PutItemRequest
+            {
+                TableName = "Projects",
+                Item = attributes
+            };
+            
+            AmazonDynamoDBClient.PutItem(request);
+            
             return View();
         }
     }
--- src/DotNetDeployments/Startup.cs
+++ src/DotNetDeployments/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Amazon.DynamoDBv2;
 
 namespace DotNetDeployments
 {
@@ -21,6 +22,7 @@ namespace DotNetDeployments
             services.AddMvc();
             services.AddDefaultAWSOptions(Configuration.GetAWSOptions());
             services.AddAWSService<IAmazonCodeDeploy>();
+            services.AddAWSService<IAmazonDynamoDB>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
