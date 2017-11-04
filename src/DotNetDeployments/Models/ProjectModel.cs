d2eb1d411cde4a41fb8431c331f9200bed5c9224
Raymond Sanchez <raymond.sanchez@verizon.com>
2017-11-04 08:14:16 -0500

    added awsAppName to project

--- src/DotNetDeployments/Models/ProjectModel.cs
+++ src/DotNetDeployments/Models/ProjectModel.cs
@@ -2,8 +2,8 @@ using Amazon.DynamoDBv2.DataModel;
 
 namespace DotNetDeployments.Models 
 {    
-   [DynamoDBTable("Projects")] 
-   public class ProjectModel 
+    [DynamoDBTable("Projects")] 
+    public class ProjectModel 
     { 
         [DynamoDBHashKey]
         public string SolutionName { get; set; } 
@@ -12,5 +12,12 @@ namespace DotNetDeployments.Models
         public string ProjectName { get; set; }
 
         public ProjectType ProjectType { get; set; }
-     } 
+
+        public string AwsAppName { get; set; }
+
+        public ProjectModel()
+        {
+            AwsAppName = $"{SolutionName}-{ProjectName}";
+        }
+    } 
  }
